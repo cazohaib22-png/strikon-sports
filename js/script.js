2171d73568a3ad87dca21358e9f6180f09d64476
cazohaib22-png <cazohaib22@gmail.com>
2026-07-19 00:21:13 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -64,14 +64,85 @@ document.querySelectorAll(".fade-in").forEach(el => {
    PRODUCT IMAGE GALLERY
 ================================*/
 
+const products = {
+
+1:{
+title:"Strikon Elite (TB-01)",
+images:[
+"../../../images/football/TB1-01.jpg",
+"../../../images/football/TB1-02.jpg",
+"../../../images/football/TB1-03.jpg",
+"../../../images/football/TB1-04.jpg"
+]
+},
+
+2:{
+title:"Strikon Pro Match (TB-02)",
+images:[
+"../../../images/football/TB2-01.jpg",
+"../../../images/football/TB2-02.jpg",
+"../../../images/football/TB2-03.jpg",
+"../../../images/football/TB2-04.jpg"
+]
+},
+
+3:{
+title:"Strikon Velocity (TB-03)",
+images:[
+"../../../images/football/TB3-01.jpg",
+"../../../images/football/TB3-02.jpg",
+"../../../images/football/TB3-03.jpg",
+"../../../images/football/TB3-04.jpg"
+]
+}
+
+};
+
 function changeImage(img){
 
 document.getElementById("mainProductImage").src = img.src;
 
-document
-.querySelectorAll(".thumbnail-gallery img")
+document.querySelectorAll(".thumbnail-gallery img")
 .forEach(i=>i.classList.remove("active"));
 
 img.classList.add("active");
+
+}
+
+function loadProduct(id){
+
+const thumbs=document.querySelectorAll(".thumbnail-gallery img");
+
+thumbs.forEach((img,index)=>{
+
+img.src=products[id].images[index];
+
+img.classList.remove("active");
+
+});
+
+thumbs[0].classList.add("active");
+
+document.getElementById("mainProductImage").src=products[id].images[0];
+
+const title=document.getElementById("productTitle");
+
+if(title){
+
+title.innerHTML=products[id].title;
+
+}
+
+const hero=document.getElementById("top-product");
+
+if(hero){
+
+hero.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
 
 }
